7df81ea481d0f1f9aaa5f8717ae59c3662bac2a4
Jazenn <jason.griffioen@student.hu.nl>
2023-02-09 15:06:11 +0100

    Fully completed assignment 3.

--- Formatieve Opdracht 1a/Opdracht 3 - Lijstcheck.py
+++ Formatieve Opdracht 1a/Opdracht 3 - Lijstcheck.py
@@ -1,9 +1,17 @@
 import random
 
+
 def lijst_genereren():
     lijst_getallen = []
     for i in range(0, 30):
-        lijst_getallen.append(random.randint(0, 10))
+        lijst_getallen.append(random.randint(-10, 10))
+    return lijst_getallen
+
+
+def lijst_1en_0en():
+    lijst_getallen = []
+    for i in range(0, 30):
+        lijst_getallen.append(random.randint(0, 1))
     return lijst_getallen
 
 
@@ -19,4 +27,34 @@ def count(x):
     return counter
 
 
+def grootste_verschil():
+    lijst_getallen = lijst_genereren().copy()
+    print(lijst_getallen)
+    verschil_getallen = 0
+    verschil_grootste = 0
+    counter = 1
+
+    for getal in lijst_getallen:
+        if counter >= len(lijst_getallen):
+            break
+        verschil_getallen = getal - lijst_getallen[counter]
+        if abs(verschil_getallen) >= verschil_grootste:
+            verschil_grootste = abs(verschil_getallen)
+        counter += 1
+
+    return verschil_grootste
+
+
+def lijst_controleren():
+    lijst_getallen = lijst_1en_0en().copy()
+    print(lijst_getallen)
+
+    if lijst_getallen.count(1) > lijst_getallen.count(0):
+        if lijst_getallen.count(0) <= 12:
+            return True
+    return False
+
+
 print(count(3))
+print(grootste_verschil())
+print(lijst_controleren())
